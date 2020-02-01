70c2326047d5966c0d722aaed537841ce3d00218
Aida Abdikulova <abdikulova@gmail.com>
2020-02-01 04:25:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,7 +6,13 @@ class Student
   #  with DB[:conn]
 
   attr_accessor :name, :grade
-  attr_reader :id 
+  attr_reader :id
+
+  def initialize(name,grade,id=nil)
+    @id = id
+    @name = name
+    @grade = grade
+  end
 
 
 end
